431967c005b155fadf5816348ba90deda6edd042
Myrroddin <psvander@gmail.com>
2024-03-10 09:48:38 -0600

    [All] There is no need to add to BetterBags' localization table. Internalize the translations to this addon

--- Core.lua
+++ Core.lua
@@ -5,17 +5,10 @@ assert(BetterBags, "BetterBags_Hearthstones requires BetterBags")
 ---@class Categories: AceModule
 local categories = BetterBags:GetModule("Categories")
 
----@class Localization: AceModule
-local L = BetterBags:GetModule("Localization")
-
 ---@type string, AddonNS
 local _, addon = ...
-local database = addon.db
-
--- start fresh, delete all items in the category; does not delete the category itself
-categories:WipeCategory(L:G(TUTORIAL_TITLE31))
 
 -- create a category and populate with itemIDs
-for index in pairs(database) do
-    categories:AddItemToCategory(index, L:G(TUTORIAL_TITLE31))
+for itemID in pairs(addon.db) do
+    categories:AddItemToCategory(itemID, TUTORIAL_TITLE31)
 end
