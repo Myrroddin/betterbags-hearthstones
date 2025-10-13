64c1efc911a71f8f09691ec01a4a243b72b4920b
Myrroddin <psvander@gmail.com>
2025-10-12 18:06:53 -0600

    [All] Reformat Lua files, replacing double and quadruple spaces with true tabs. In other words, clean up whitespaces.

--- Core.lua
+++ Core.lua
@@ -1,15 +1,10 @@
----@diagnostic disable: undefined-field
----@class BetterBags: AceAddon
 local BetterBags = LibStub("AceAddon-3.0"):GetAddon("BetterBags")
 assert(BetterBags, "BetterBags_Hearthstones requires BetterBags")
 
----@class Categories: AceModule
 local categories = BetterBags:GetModule("Categories")
 
----@class Localization: AceModule
 local L = BetterBags:GetModule("Localization")
 
----@type string, table
 local _, addon = ...
 
 -- wipe Teleporters category, as it does slmost the same thing, just with fewer items in its DB
@@ -17,5 +12,5 @@ categories:WipeCategory("Teleporters")
 
 -- create a category and populate with itemIDs
 for itemID in pairs(addon.db) do
-    categories:AddItemToCategory(itemID, L:G(TUTORIAL_TITLE31))
+	categories:AddItemToCategory(itemID, L:G(TUTORIAL_TITLE31))
 end
