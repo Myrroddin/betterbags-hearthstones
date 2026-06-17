30049772bba88647a497041be343de7a5d6b9136
Myrroddin <psvander@gmail.com>
2026-06-17 00:04:32 -0600

    [Retail] Bump ToC for patch 12.0.7

--- Core.lua
+++ Core.lua
@@ -2,16 +2,17 @@ local BetterBags = LibStub("AceAddon-3.0"):GetAddon("BetterBags")
 assert(BetterBags, "BetterBags_Hearthstones requires BetterBags")
 
 local categories = BetterBags:GetModule("Categories")
-
-local L = BetterBags:GetModule("Localization")
+local BL = BetterBags:GetModule("Localization")
+local context = BetterBags:GetModule("Context")
+local ctx = context:New("BetterBags_Hearthstones")
 
 local _, addon = ...
 
--- wipe Teleporters and BearLeeAble's Ultimate catagories, as they do the same thing, just with fewer items in their databases
-categories:WipeCategory("Teleporters")
-categories:WipeCategory("|cff16A0FFHearthstones|r")
+-- wipe Teleporters and BearLeeAble's Ultimate categories, as they duplicate this category with fewer items
+categories:WipeCategory(ctx, BL:G("Teleporters"))
+categories:WipeCategory(ctx, BL:G("|cff16A0FFHearthstones|r"))
 
 -- create a category and populate with itemIDs
 for itemID in pairs(addon.db) do
-	categories:AddItemToCategory(itemID, L:G(TUTORIAL_TITLE31))
+	categories:AddItemToCategory(ctx, itemID, BL:G(TUTORIAL_TITLE31))
 end
